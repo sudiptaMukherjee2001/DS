c8f9625753203d55b90085c5340fb098fd40bb55
sudiptaMukherjee2001 <frontendDeveloperabbs@gmail.com>
2024-09-02 22:14:35 +0530

    Insert at First Func created

--- linkedlist/Singaly-likendList/InstertionOp.c
+++ linkedlist/Singaly-likendList/InstertionOp.c
@@ -30,6 +30,29 @@ return head;
 
 }
 
+
+
+struct InstertionOp*  insertAtFront(struct InstertionOp* head ){
+    struct InstertionOp* newNode;
+    struct InstertionOp* temp;
+    newNode=(struct InstertionOp*)malloc(sizeof(struct InstertionOp));
+    newNode->link=NULL;
+    printf("Enter the data==>");
+    scanf("%d",&newNode->data);
+    temp=head;
+    if(head!=NULL){
+        newNode->link=temp;
+        temp=newNode;
+    }else{
+        printf("List is empty .. Please create a list is ==>");
+    }
+    head=temp;
+    printf("\ninside insert function ==> %d",head->data);
+    printf("\n");
+    return head;
+
+}
+
 void traverseList(struct InstertionOp* head ){
     struct InstertionOp* temp;
     temp=head;
@@ -43,14 +66,12 @@ void traverseList(struct InstertionOp* head ){
 }
 
 
-
-
-
 int main() {
     struct InstertionOp* head = NULL;
+    struct InstertionOp* temp;
     int ch;
     while (1){
-        printf("\n1.Insert\n2.Traverse\n3.Exit\n");
+        printf("\n1.Insert\n2.Traverse\n3.Insert at start\n4.Insert at middel\n5.Insert at end\n6.Exit");
         printf("\n====================================");
         printf("\nEnter the choice==>");
         scanf("%d",&ch);
@@ -59,6 +80,17 @@ int main() {
         }else if(ch==2){
             traverseList(head);
         }else if(ch==3){
+            head =insertAtFront(head); // here  we store newNode address in left side of head variable ..
+            temp=head;
+            printf("Insert list==>\n");
+
+           while (temp!=NULL){
+            printf("%d\t",temp->data);
+            temp=temp->link;
+           }
+            printf("Null");           
+            printf("\nthis is head data %d\t",head->data);
+        }else if(ch==6){
             break;
         }else{
             printf("Invalid choice");
